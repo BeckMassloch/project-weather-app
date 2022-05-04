717d23c89a0b246a192c3aeba46f40eefaa57004
BeckMassloch <100127654+BeckMassloch@users.noreply.github.com>
2022-05-04 23:08:15 +1000

    Added current time and temperature

---
+++ src/app.js
@@ -0,0 +1,46 @@
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#weather-description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+function displayDate() {
+  let now = new Date();
+  let weekdays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let currentDay = weekdays[now.getDay()];
+  let currentHour = now.getHours();
+  if (currentHour < 10) {
+    currentHour = `0${currentHour}`;
+  }
+  let currentMinutes = now.getMinutes();
+  if (currentMinutes < 10) {
+    currentMinutes = `0${currentMinutes}`;
+  }
+  let dateElement = document.querySelector("#current-date");
+  dateElement.innerHTML = `${currentDay} ${currentHour}:${currentMinutes}`;
+}
+
+let apiKey = "3153a29ca3b931fcc59027a2462c1744";
+let units = "metric";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=berlin&units=${units}&appid=${apiKey}`;
+console.log(apiUrl);
+
+displayDate();
+
+axios.get(apiUrl).then(displayTemperature);
